66112a0607b632ef35b0175a233e7ff33c445c26
Lucas Castelan Prado <castelanprado@gmail.com>
2017-03-02 17:35:40 -0300

    twitter buttom fixed

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
         success: function(data) {
           $('#quote').text(data.quote);
           $('#author').text(data.author);
-          $('#tweet').attr("href", 'https://twitter.com/intent/tweet?text=' + $('#quote').html() + "-" + data.author);
+          $('#tweet').attr("href", 'https://twitter.com/intent/tweet?text=' + data.quote + " " + "-" + " " + data.author);
         },
         beforeSend: function(xhr) {
             xhr.setRequestHeader("X-Mashape-Authorization", "Ey82Qs9cYemshOTkDjmdGHedDKT7p1lD10ljsntsa1dNilbP8V");
@@ -26,19 +26,3 @@
         }
     });
   }
-
-  //function newTweet(data) {
-  //  $('#tweet-share-buttom').attr("href", 'https://twitter.com/intent/tweet?text=' + data.quote + ' -' + data.author);
-  //}
-
-  twttr.widgets.createShareButton(
-  "https:\/\/dev.twitter.com\/web\/tweet-button",
-  document.getElementById("tweet-container"),
-  {
-    size: "large",
-    text: "custom share text",
-    hashtags: "example,demo",
-    via: "twitterdev",
-    related: "twitterapi,twitter"
-  }
-);
